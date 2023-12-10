9792d7f446f6d922763deedacb96744808c3c067
DawnEggleton <dawn.eggleton@carleton.ca>
2023-12-10 19:26:01 -0400

    v2 push

---
+++ source/js/settings.js
@@ -0,0 +1,8 @@
+const threadSheet = `1XNHsAlHv62PHeXuyWfbegNueYi9QdeyvUPlJ0Qu64i0`;
+const threadDeploy = `AKfycbzeDxYwhTWUP-tLkW_kWaqOKTR6IJ_EWknJCW8hVyrC2wTdimIDqmilpIZRVmC6SeNh`;
+const characterSheet = `1uqiL_nOlZ1s3Nf5Z9YPkuzcX79KSfJTrVkLhShQ0koU`;
+
+const current = new Date();
+const year = current.getFullYear();
+const month = current.getMonth() + 1;
+const day = current.getDate();
